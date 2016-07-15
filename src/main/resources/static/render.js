a20b17403f8aeb59b57495ba7b8146e684fc569d
Marc Christie <mchristie@walkerinfo.com>
2016-07-15 18:36:01 -0400

    Removing unprintable Unicode characters

--- src/main/resources/static/render.js
+++ src/main/resources/static/render.js
@@ -9,17 +9,17 @@ function renderJsx(template, model) {
     return render(jsTemplate, data);
 }
 
-// Create a real JSON object from the model Map 
+// Create a real JSON object from the model Map
 function toJsonObject(model) {
-    var o = {}; 
-    for (var k in model) { 
+    var o = {};
+    for (var k in model) {
         // Convert Iterable like List to real JSON array
         if (model[k] instanceof Java.type("java.lang.Iterable")) {
             o[k] = Java.from(model[k]);
         }
         else {
-            o[k] = model[k]; 
-        } 
-    }  
+            o[k] = model[k];
+        }
+    }
     return o;
 }
